68cf532005c119addcb2e228aa91c67a4ee0bfab
Timo T <60548494+Ultsi00@users.noreply.github.com>
2023-09-25 12:56:22 +0300

    Add files via upload

--- includes/embed.h
+++ includes/embed.h
@@ -1,6 +1,7 @@
 #ifndef EMBED_H
 # define EMBED_H
 # include <SDL2/SDL.h>
+# include <SDL2/SDL_image.h>
 # include <iostream>
 # include "classes.h"
 const int   SCREEN_WIDTH = 1280;
@@ -12,11 +13,15 @@ typedef struct s_sdl {
     SDL_Window  *window;
     SDL_Surface *surface;
     SDL_Event   event;
+    SDL_Renderer *renderer;
     bool        quit;
+    SDL_Texture *s_controller;  //think if smart to put textures in classes, else to sub-struct
 }   t_sdl;
 
 void    prgm_loop(Parameter& param, t_sdl *sdl);
 void    sdl_init(t_sdl *sdl);
-void    exit_prgm(t_sdl *sdl);
+void    exit_prgm(t_sdl *sdl, string error_msg);
+void    key_events(t_sdl *sdl); // necessary to send whole struct?
+void    load_media(t_sdl *sdl);
 
 #endif
